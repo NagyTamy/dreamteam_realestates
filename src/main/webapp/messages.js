db454149f6b262d5268c39ca4a4e71b7ed9a5553
NagyTamy <nagy.tamy@gmail.com>
2019-06-06 17:01:29 +0200

    CSS updated for message history

--- src/main/webapp/messages.js
+++ src/main/webapp/messages.js
@@ -1,3 +1,20 @@
 function onLoadConversation(listOfMessages) {
 
 }
+
+/*<div id="container">
+    <div id="message-history"> <!-- set to scroll to bottom on load! -->
+        <div class="incoming">
+            <p class="content">Content goes here</p>
+            <p class="tooltip-incoming">time goes here</p> <!--set tooltip-message class, trigger to make this visible in js-->
+        </div>
+        <div class="outgoing">
+            <p class="content">Content goes here</p>
+            <p class="tooltip-outgoing">time goes here</p> <!--set tooltip-message class trigger to make this visible in js-->
+        </div>
+    </div>
+    <div id="new-message">
+        <textarea name="new-message" rows="2" placeholder="Type your answer here..."></textarea>
+        <button class="button-right">Send</button>
+    </div>
+</div>*/
